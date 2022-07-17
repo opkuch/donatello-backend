d03bdbc1c670ddfe0b3a2f60e92c069ca22f790c
Nadav Ben Hur <nadi2022@gmail.com>
2022-07-17 15:09:58 +0300

    .

--- api/auth/auth.service.js
+++ api/auth/auth.service.js
@@ -13,8 +13,8 @@ async function login(username, password) {
     // if (!user) return Promise.reject('Invalid username or password')
     if (!user) throw new Error('Invalid username or password')
     // TODO: un-comment for real login
-    // const match = await bcrypt.compare(password, user.password)
-    // if (!match) return Promise.reject('Invalid username or password')
+    const match = await bcrypt.compare(password, user.password)
+    if (!match) return Promise.reject('Invalid username or password')
 
     delete user.password
     return user
